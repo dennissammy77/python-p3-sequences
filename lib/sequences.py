8a3f16499dc589be6253e42881020925c598c650
dennissammy77 <dennissammy77@gmail.com>
2025-05-08 10:21:00 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    int_list = list()
+    
+    # first two terms
+    n1, n2 = 0, 1
+    count = 0
+    
+    while count < length:
+       int_list.append(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
     pass
+    print(int_list)
+
+print_fibonacci(9)
